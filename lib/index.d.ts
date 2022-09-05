b84e99894c124da43a4390bc973a15bb883dbc1e
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2022-09-05 12:46:00 +0300

    Improve validateTCID, add in-code docs

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,10 @@
-export declare const validateTCID: (id: string | number) => string;
+declare type tcIdFormat = "string" | "number";
+/**
+ * @func validateTCID
+ * @desc A function to validate TC ID numbers. It throws error or returns the id number.
+ * @param id `string` | `number`
+ * @param returnType `tcIdFormat` - `String` or `Number`
+ * @returns Returns id number only if it is valid. type is either string or number based on returnType param.
+ */
+export declare const validateTCID: (id: string | number, returnType?: tcIdFormat) => string | number;
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,31 @@
-export const validateTCID = (id: string | number) => {
-  let tc_id: string;
+type tcIdFormat = "string" | "number";
 
-  if (typeof id != "string") tc_id = String(id);
-  else tc_id = id;
+/**
+ * @func validateTCID
+ * @desc A function to validate TC ID numbers. It throws error or returns the id number.
+ * @param id `string` | `number`
+ * @param returnType `tcIdFormat` - `String` or `Number`
+ * @returns Returns id number only if it is valid. type is either string or number based on returnType param.
+ */
+export const validateTCID = (
+  id: string | number,
+  returnType: tcIdFormat = "string"
+) => {
+  if (typeof id != "string") id = String(id);
 
   // Turkish identity number consists of eleven characters, all digits.
-  if (!/^[0-9]{11}$/.test(tc_id)) {
-    throw new Error("TC ID is not valid");
+  if (!/^[0-9]{11}$/.test(id)) {
+    throw new Error("TC ID is not valid: " + id);
   }
 
   // First 9 digits of tc id is used to calculate last 2 fields
-  const first9 = tc_id.slice(0, 9);
+  const first9 = id.slice(0, 9);
 
   // oddSum is calculated using even indexes since first index starts with 0 in js
   const oddSum: number = first9
     .split("")
     .map((char) => Number(char))
-    .filter((num, idx) => {
+    .filter((_, idx) => {
       if (idx % 2 == 0) return true;
       else return false;
     })
@@ -26,7 +35,7 @@ export const validateTCID = (id: string | number) => {
   const evenSum: number = first9
     .split("")
     .map((char) => Number(char))
-    .filter((num, idx) => {
+    .filter((_, idx) => {
       if (idx % 2 == 1) return true;
       else return false;
     })
@@ -36,13 +45,15 @@ export const validateTCID = (id: string | number) => {
   const digit10th: string = String((oddSum * 7 + evenSum * 9) % 10);
   const digit11th: string = String((oddSum * 8) % 10);
 
-  if (digit10th != tc_id[9]) {
-    throw new Error("TC ID is not valid");
+  if (digit10th != id[9]) {
+    throw new Error("TC ID is not valid: " + id);
   }
 
-  if (digit11th != tc_id[10]) {
-    throw new Error("TC ID is not valid");
+  if (digit11th != id[10]) {
+    throw new Error("TC ID is not valid: " + id);
   }
 
-  return tc_id;
+  if (returnType && returnType == "number") id = Number(id);
+
+  return id;
 };
